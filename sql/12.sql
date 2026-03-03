d8ecf2bbe964da8988ae8f7bedf1fbf029c98f49
Subigya Baral <sbaral@cmc.edu>
2026-03-03 09:57:18 -0800

    Output

--- sql/12.sql
+++ sql/12.sql
@@ -12,4 +12,4 @@ FROM customer AS c
 JOIN payment AS p
   ON c.customer_id = p.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
-ORDER BY c.last_name;
+ORDER BY c.last_name, c.first_name, c.customer_id;
