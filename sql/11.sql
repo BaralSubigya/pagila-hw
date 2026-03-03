757d44905934380260d5ad71387e9af6c5a874c6
Subigya Baral <sbaral@cmc.edu>
2026-03-03 09:42:53 -0800

    Complete pagila homework solutions

--- sql/11.sql
+++ sql/11.sql
@@ -3,3 +3,13 @@
  * Use tables inventory and film.
  * Order by film title in reverse alphabetical order.
  */
+SELECT
+  f.film_id,
+  f.title,
+  COUNT(i.inventory_id) AS count
+FROM film AS f
+JOIN inventory AS i
+  ON f.film_id = i.film_id
+WHERE f.title LIKE 'H%'
+GROUP BY f.film_id, f.title
+ORDER BY f.title DESC;
