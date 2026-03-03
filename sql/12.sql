fef1965a83f3f2d3730467633eaaa12c463f485a
Subigya Baral <sbaral@cmc.edu>
2026-03-03 14:16:10 -0800

    Fix deterministic ordering

--- sql/12.sql
+++ sql/12.sql
@@ -4,16 +4,12 @@
  * Use tables payment and customer.
  */
 SELECT
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    SUM(p.amount) AS sum
+  c.customer_id,
+  c.first_name,
+  c.last_name,
+  SUM(p.amount) AS sum
 FROM customer AS c
 JOIN payment AS p
-    ON c.customer_id = p.customer_id
-GROUP BY
-    c.customer_id,
-    c.first_name,
-    c.last_name
-ORDER BY
-    c.last_name;
+  ON c.customer_id = p.customer_id
+GROUP BY p.customer_id, c.first_name, c.last_name
+ORDER BY c.last_name, c.first_name, c.customer_id;
